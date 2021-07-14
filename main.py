d7457617bb14ee0c76c17774aa823685dc1c2400
beka1cfc <buloiyn@gmail.com>
2021-07-14 17:06:34 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#задача 2
+print("Hello I am programmer")
+
+
+#задача 3
+name = "Beksultan"
+print("My name is " + name)
+
+
+#задача 4
+a = "103"
+b = 301
+Not_found = int(a) + b
+print(Not_found)
